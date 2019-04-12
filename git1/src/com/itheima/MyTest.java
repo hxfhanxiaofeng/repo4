7ec311d2da4aa18c08ea1f55756c02db1e4c6283
hanxiaonfeng <hanxiaofeng@163.com>
2019-04-12 21:12:16 +0800

    the first commit

---
+++ git1/src/com/itheima/MyTest.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/**
+ * @Classname MyTest
+ * @Description TODO
+ * @Date 2019/4/12 21:11
+ * @Created by hanxiaofeng
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        System.out.println("1111111111111111");
+    }
+}
